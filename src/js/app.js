c11dad11be719d0d2f4e3fff9ab75ac96eead178
Gil Fewster <gil.fewster@artprocessors.net>
2021-07-20 17:32:57 +1000

    grid

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,27 @@
-console.log('Hello World');
+const STATE_TOGGLE = {
+  ATTRIBUTE: "data-toggle-state",
+  PROP: "toggleState",
+};
+const TOGGLE_VALUE = { OPEN: "open", CLOSED: "closed" };
 
-const obj = {
-  arr: [1, 2, 3, 4],
-  printArr() {
-    console.log(...this.arr);
-  },
+const toggleDrawer = (e) => {
+  const state = e.target.dataset[STATE_TOGGLE.PROP];
+  e.target.dataset[STATE_TOGGLE.PROP] =
+    state === TOGGLE_VALUE.OPEN ? TOGGLE_VALUE.CLOSED : TOGGLE_VALUE.OPEN;
 };
 
-obj.printArr();
+const initToggles = () => {
+  Array.from(document.querySelectorAll(`[${STATE_TOGGLE.ATTRIBUTE}]`)).map(
+    (el) => (el.onclick = toggleDrawer)
+  );
+};
+
+const start = () => {
+  initToggles();
+
+  // initNavToggle(document.querySelectorAll("nav")[0]);
+};
+
+(() => {
+  start();
+})();
